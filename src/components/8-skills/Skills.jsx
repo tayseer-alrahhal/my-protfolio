5e67c39daa0c543c3f838201e67c750698c3899b
tayseer-alrahhal <tayseer.alrahhal.dev@gmail.com>
2025-05-11 01:09:33 +0300

    Edit on Skills Page

--- src/components/8-skills/Skills.jsx
+++ src/components/8-skills/Skills.jsx
@@ -10,30 +10,33 @@ export default function Skills() {
       <div className="skills-section">
         <div className="skill-word">SKILLS</div>
         <div className="icons-container">
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/css-3.png" alt="" />
+          <div className="skills-icon" data-skill="CSS3">
+            <img width={"40px"} src="/skills/css-3.png" alt="CSS3" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/html.png" alt="" />
+          <div className="skills-icon" data-skill="HTML5">
+            <img width={"40px"} src="/skills/html.png" alt="HTML5" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/js.png" alt="" />
+          <div className="skills-icon" data-skill="JavaScript">
+            <img width={"40px"} src="/skills/js.png" alt="JavaScript" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/idea.png" alt="" />
+          <div className="skills-icon" data-skill="Typescript">
+            <img width={"40px"} src="/skills/typescript.png" alt="Typescript" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/redux.1024x971.png" alt="" />
+          <div className="skills-icon" data-skill="React">
+            <img width={"40px"} src="/skills/structure.png" alt="React" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/Tailwind CSS.png" alt="" />
+          <div className="skills-icon" data-skill="Next.js">
+            <img width={"40px"} src="/skills/next.js.png" alt="Next.js" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/structure.png" alt="" />
+          <div className="skills-icon" data-skill="Tailwind CSS">
+            <img width={"40px"} src="/skills/Tailwind CSS.png" alt="Tailwind CSS" />
           </div>
-          <div className="skills-icon">
-            <img width={"40px"} src="/skills/git.png" alt="" />
+          <div className="skills-icon" data-skill="Git">
+            <img width={"40px"} src="/skills/git.png" alt="Git" />
           </div>
+          {/* <div className="skills-icon" data-skill="Problem solving">
+            <img width={"40px"} src="/skills/idea.png" alt="Problem solving" />
+          </div> */}
         </div>
       </div>
 
